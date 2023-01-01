63c215ef7767af6ae952ec1ea183110312dd81a1
Matthieu Mota <matthieumota@gmail.com>
2023-01-01 15:42:10 +0100

    Menu close mobile

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-console.log('I like JavaScript');
+// Show/hide menu
+const navMenu = document.querySelector('.menu');
+const navToggle = document.querySelector('.menu-toggle');
+const navClose = document.querySelector('.menu-close');
+
+navToggle.addEventListener('click', () => navMenu.classList.add('show'));
+navClose.addEventListener('click', () => navMenu.classList.remove('show'));
