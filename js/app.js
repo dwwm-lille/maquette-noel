b830cb01fa69999f30ee29f3781e68be6d7824dd
Matthieu Mota <matthieumota@gmail.com>
2023-01-01 21:13:21 +0100

    Dark mode js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
+// Shadow menu
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+
+    if (window.scrollY >= 50) {
+        header.classList.add('shadow');
+    } else {
+        header.classList.remove('shadow');
+    }
+});
+
+// Dark mode
+const themeButton = document.querySelector('.theme i');
+
+// Avec ça, on peut avoir le thème dark par défaut...
+if (document.body.classList.contains('dark')) {
+    themeButton.classList.remove('bx-moon');
+    themeButton.classList.add('bx-sun');
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    themeButton.classList.toggle('bx-moon');
+    themeButton.classList.toggle('bx-sun');
+});
+
 // Show/hide menu
 const navMenu = document.querySelector('.menu');
 const navToggle = document.querySelector('.menu-toggle');
